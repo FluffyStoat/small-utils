550c267ae327ff7758ba14798a3f1cd550847a2c
Konstantin Bochkarev <bkv.developer@gmail.com>
2020-11-15 16:56:54 +0400

    eel has been removed kivy has been added

--- access_generator/app.py
+++ access_generator/app.py
@@ -1,11 +1,22 @@
 import os
-
+import kivy
 import javalang
-import eel
 
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+
+from os import walk, path
 from typing import List, Dict
 from javalang.tree import CompilationUnit
-from os import walk, path
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
+
+kivy.require('2.0.0')  # replace with your current kivy version !
 
 
 def close_callback(route, websockets):
@@ -13,7 +24,6 @@ def close_callback(route, websockets):
         exit()
 
 
-@eel.expose
 def get_class_list():
     modifiers: List[str] = ["private", "static", "final"]
 
@@ -46,14 +56,61 @@ def get_class_list():
     return ["Driver", "Customer", "Order", "Account", "Building", "MasterRequest"]
 
 
-@eel.expose
-def generate(values: List[str]) -> List[str]:
-    for val in values:
-        print(f"val: {val}")
+def on_checkbox_active(checkbox, value):
+    if value:
+        print('The checkbox', checkbox, 'is active')
+    else:
+        print('The checkbox', checkbox, 'is inactive')
 
-    return ["result1", "result2", "result3", "result4"]
+
+class MainScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+
+        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+        # Make sure the height is such that there is something to scroll.
+        layout.bind(minimum_height=layout.setter('height'))
+
+        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        root.add_widget(layout)
+        self.add_widget(root)
+        self.cols = 1
+        self.rows = 1
+
+        for cls in get_class_list():
+
+            row_layout: BoxLayout = BoxLayout(orientation='horizontal', size_hint_y=None, height=50)
+            layout.add_widget(row_layout)
+
+            class_layout: BoxLayout = BoxLayout(orientation='horizontal')
+
+            check_box: CheckBox = CheckBox()
+            check_box.color = [255, 250, 250, 1]
+
+            class_layout.add_widget(check_box)
+            class_layout.add_widget(Label(text=f'[b]{cls}[/b]', markup=True))
+            row_layout.add_widget(class_layout)
+
+            operations: List[str] = ['Создать', 'Просмотр', 'Редактировать', 'Удалить']
+
+            for oper in operations:
+                operations_layout = BoxLayout(orientation='horizontal')
+
+                check_box: CheckBox = CheckBox()
+                check_box.color = [255, 250, 250, 1]
+                check_box.bind(active=on_checkbox_active)
+
+                operations_layout.add_widget(check_box)
+                operations_layout.add_widget(Label(text=f"{oper}"))
+                row_layout.add_widget(operations_layout)
+
+
+class GeneratorApp(App):
+
+    def build(self):
+        return MainScreen()
 
 
 if __name__ == '__main__':
-    eel.init('web')
-    eel.start('main.html', size=(800, 600), port=31001, close_callback=close_callback)
+    GeneratorApp().run()
